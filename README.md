40a1a4afa409fb8886d18d4ae33c16cc7b820ca2
Takuma-Ikeda <el.programdear@gmail.com>
2022-08-01 03:24:34 +0900

    feat: ローカルで動作確認したところまで

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # GoogleCalendar-Notification
+
+- xxx
+
+## 動作環境
+
+```sh
+pipenv install
+pipenv shell
+```
+
+- Python 3.9.6
+
+### Library
+
+- [google-api-python-client](https://github.com/googleapis/google-api-python-client)
+- [google-auth-httplib2](https://github.com/googleapis/google-auth-library-python-httplib2)
+- [google-auth-oauthlib](https://github.com/googleapis/google-auth-library-python-oauthlib)
---
+++ google_calendar_notification_for_slack.py
@@ -0,0 +1,18 @@
+from my_google_calendar import MyGoogleCalendar
+
+
+class GoogleCalendarNotificationForSlack:
+
+    def run(self):
+        google_calendar = MyGoogleCalendar()
+
+        for name, calendar_id in google_calendar.config.calendar_ids.items():
+            text = google_calendar.get_str_next_schedule(calendar_id, name)
+
+            if text is not None:
+                # google_calendar.slack_post(text, 'Next Calendar TODO', ':calendar:', None)
+                print('Slack Post About GoogleCalendar Done!')
+
+
+google_calendar_notification_for_slack = GoogleCalendarNotificationForSlack()
+google_calendar_notification_for_slack.run()
